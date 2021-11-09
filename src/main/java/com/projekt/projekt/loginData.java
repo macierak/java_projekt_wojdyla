681525cb324937ec23c307962aadf57413713109
Maciej Wojdyła <24899@student.kpu.krosno.pl>
2021-11-09 18:55:08 +0100

    initial draft

---
+++ src/main/java/com/projekt/projekt/loginData.java
@@ -0,0 +1,19 @@
+package com.projekt.projekt;
+
+public class loginData {
+    String login, password;
+
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
